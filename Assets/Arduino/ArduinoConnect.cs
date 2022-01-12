1d2e23b7daaa2fd41bfb86a3aa4171434f3c3ce7
Lars Mulder <larsmulderrr@gmail.com>
2022-01-12 21:34:21 +0100

    arduino code push

---
+++ Assets/Arduino/ArduinoConnect.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class ArduinoConnect : MonoBehaviour
+{
+    private SerialPort sp;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        StartCoroutine(Search());
+    }
+
+    private void LateUpdate()
+    {
+        if (sp != null)
+        {
+            if (sp.IsOpen)
+            {
+                if (sp.BytesToRead != 0)
+                {
+                    int serialCommand = Convert.ToInt32(sp.ReadByte());
+                    int serialParameter = 0;
+                    int serialEnding;
+                    bool serialRead = false;
+                    if (serialCommand > 0 && serialCommand <= 5)
+                    {
+                        serialParameter = Convert.ToInt32(sp.ReadByte()); //read the second byte coming in
+                        if (serialParameter > 0 && serialParameter <= 255)
+                        {
+                            serialEnding = Convert.ToInt32(sp.ReadByte()); //read the ending byte to assemble the complete command
+                            if (serialEnding == 0)
+                            {
+                                serialRead = true;
+                            }
+                        }
+                    }
+
+                    if (serialRead)
+                    {
+                        switch (serialCommand)//run the command sent from Unity
+                        {
+                            case 1://dash sensor
+                                Debug.Log("dash" + serialParameter);
+                                break;
+                            case 2://jump sensor
+                                Debug.Log("jump" + serialParameter);
+                                break;
+                            case 3://shield sensor
+                                Debug.Log("shield" + serialParameter);
+                                break;
+                        }
+                    }
+
+                }
+
+            }
+        }
+    }
+
+    private IEnumerator Search()
+    {
+        yield return new WaitForSeconds(1);
+        while (sp == null)
+        {
+            foreach (string str in SerialPort.GetPortNames())
+            {
+                Debug.Log(string.Format("Existing COM port: {0}", str));
+                if (str == "COM4")
+                {
+                    sp = new SerialPort("COM4", 9600);
+                    sp.Open();
+                    sp.ReadTimeout = 1;
+                    Debug.Log("ARDUINO FOUND");
+                }
+            }
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
---
+++ Assets/Arduino/ArduinoSensorCode/ArduinoSensorCode.ino
@@ -0,0 +1,66 @@
+const int trigPin = 4;
+const int echoPin = 2;
+const int distance_threshold = 30; //cm
+
+const int trigPin2 = 8;
+const int echoPin2 = 7;
+const int distance_threshold2 = 30; //cm
+
+const int trigPin3 = 13;
+const int echoPin3 = 12;
+const int distance_threshold3 = 30; //cm
+
+float duration_us, duration_us2, duration_us3, distance_cm, distance_cm2, distance_cm3;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+
+  pinMode(trigPin3, OUTPUT);
+  pinMode(echoPin3, INPUT);
+}
+
+void loop() {
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration_us = pulseIn(echoPin, HIGH);
+  distance_cm = 0.017 * duration_us;
+
+  digitalWrite(trigPin2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin2, LOW);
+
+  duration_us2 = pulseIn(echoPin2, HIGH);
+  distance_cm2 = 0.017 * duration_us2;
+
+  digitalWrite(trigPin3, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin3, LOW);
+
+  duration_us3 = pulseIn(echoPin3, HIGH);
+  distance_cm3 = 0.017 * duration_us3;
+
+  if (distance_cm < distance_threshold) {
+    byte cmd[] = {1, (int)distance_cm, 0};
+    Serial.write(cmd, sizeof(cmd));
+  }
+
+  if (distance_cm2 < distance_threshold2) {
+    byte cmd[] = {2, (int)distance_cm2, 0};
+    Serial.write(cmd, sizeof(cmd));
+  }
+
+  if (distance_cm3 < distance_threshold3) {
+    byte cmd[] = {3, (int)distance_cm3, 0};
+    Serial.write(cmd, sizeof(cmd));
+  }
+
+  delay(100);
+}
